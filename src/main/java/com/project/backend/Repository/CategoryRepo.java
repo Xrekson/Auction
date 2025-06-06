824b2fef3bff4319069a9eb527589c594f138f1b
Xrekson <indraneel1080brahma@gmail.com>
2025-06-06 13:37:44 +0530

    Maven removed

--- src/main/java/com/project/backend/Repository/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.project.backend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.backend.Entity.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer> {
-
-}
--- src/main/java/com/project/backend/Repository/ListingRepo.java
+++
@@ -1,9 +0,0 @@
-package com.project.backend.Repository;
-
-import com.project.backend.Entity.Listing;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ListingRepo extends JpaRepository<Listing, Integer> {
-}
